035cc69caa8f9469eac0acccf27a88d5963ef682
leoapost <leoapost@gmail.com>
2013-10-09 19:07:42 +0300

    add facebook login and fix devise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,15 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+
+    if user.persisted?
+      sign_in_and_redirect user, event: :authentication
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: auth["provider"] if is_navigational_format?
+    else
+      session["devise.provider_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url
+    end
+
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,38 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook, :twitter]
 
   def self.create_with_omniauth(auth)
-    puts text: auth.inspect
+    provider = auth["provider"]
+    uid = auth["uid"]
 
-    User.create(email: auth["info"]["email"],
-                password: auth["info"]["email"],
-                provider: auth["provider"],
-                uid: auth["uid"],
-                name: auth["info"]["name"]
+    if provider === 'facebook'
+      email = auth["info"]["email"]
+      name = auth["info"]["name"]
+    else
+      name = "John Doe" #TODO: replace it with the twitter name
+      email = "Johndoe@example.org" #NOTICE: Twitter API does not return email
+    end
+    password = Devise.friendly_token[0,20]
+
+    User.create!(email: email,
+                password: password,
+                provider: provider,
+                uid: uid,
+                name: name
     )
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+
+      if data = session["devise.provider_data"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
 end
