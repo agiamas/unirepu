9f8d5873b04a65df458deab583348530abad53e2
leoapost <leoapost@gmail.com>
2013-10-09 13:21:56 +0300

    halfway twitter integration, with controller checked in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Unirepu::Application.routes.draw do
   root :to => "home#index"
+  match "/auth/:provider/callback" => "sessions#create", via: [:get, :post]
+
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
