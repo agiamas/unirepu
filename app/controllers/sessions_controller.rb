d1797d9e20a002f3876f8b08e7a2d6d4ea1276a4
alexandros giamas <agiamas@gmail.com>
2013-10-09 19:36:54 +0300

    fixed sessions controlelr

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,18 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
 
     if user.persisted?
-      sign_in_and_redirect user, event: :authentication
+      sign_in user, event: :authentication
+      session[:user_id] = user.id
+      session[:account_token] = auth['credentials']['token']
+      session[:account_token_secret] = auth['credentials']['secret']
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: auth["provider"] if is_navigational_format?
+
+      return redirect_to '/dashboard/index', :notice => "Signed in!"
     else
       session["devise.provider_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
     end
 
-    session[:user_id] = user.id
-    session[:account_token] = auth['credentials']['token']
-    session[:account_token_secret] = auth['credentials']['secret']
 
-    redirect_to '/dashboard/index', :notice => "Signed in!"
   end
 end
